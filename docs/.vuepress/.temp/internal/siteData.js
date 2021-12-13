eb2784e8ec8bf6e8d3b45fe39bbc98ae8d095a40
Alexander Paulino <alexanderpauul@hotmail.com>
2021-12-12 22:16:20 -0500

    first commit

---
+++ docs/.vuepress/.temp/internal/siteData.js
@@ -0,0 +1,8 @@
+export const siteData = {
+  "base": "/vuepress-documentation/",
+  "lang": "en-US",
+  "title": "Hello, VuePress!",
+  "description": "This is my first VuePress site",
+  "head": [],
+  "locales": {}
+}
---
+++ docs/.vuepress/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    dest: 'docs',
+    base: '/vuepress-documentation/',
+    lang: 'en-US',
+    title: 'Hello, VuePress!',
+    description: 'This is my first VuePress site',
+
+    themeConfig: {
+        logo: 'https://vuejs.org/images/logo.png',
+    },
+}
